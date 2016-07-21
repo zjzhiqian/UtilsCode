83c1be82cc329d9c6cb30e7dbba15ebd8a7b421e
hzq <hzq@hzqdeMacBook-Pro.local>
2016-07-21 23:00:39 +0800

    新增messagePack 编解码支持

---
+++ src/test/java/com/hzq/nio/encodeDecode/msgPack/MsgPackDecoder.java
@@ -0,0 +1,23 @@
+package com.hzq.nio.encodeDecode.msgPack;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageDecoder;
+import org.msgpack.MessagePack;
+
+import java.util.List;
+
+/**
+ * Created by hzq on 16/7/21.
+ */
+public class MsgPackDecoder extends MessageToMessageDecoder<ByteBuf> {
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
+        int length = msg.readableBytes();
+        byte[] array = new byte[length];
+        msg.getBytes(msg.readerIndex(),array,0,length);
+        MessagePack messagePack = new MessagePack();
+        out.add(messagePack.read(array));
+    }
+}
