1c122adb6f26b6bd726c60d15a61750b5fc719da
hzq <hzq@hzqdeMacBook-Air.local>
2016-07-26 17:39:32 +0800

    update CLHLock

--- src/test/java/com/hzq/lock/CLHLock.java
+++ src/test/java/com/hzq/lock/CLHLock.java
@@ -3,7 +3,7 @@ package com.hzq.lock;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
-public class CHKLock {
+public class CLHLock {
 
     /**
      * Created by hzq on 16/5/14.
@@ -18,7 +18,7 @@ public class CHKLock {
     private ThreadLocal<QNode> myNode;
     private ThreadLocal<QNode> myPre;
 
-    public CHKLock() {
+    public CLHLock() {
         myNode = new ThreadLocal<QNode>() {
             @Override
             protected QNode initialValue() {
@@ -53,7 +53,7 @@ public class CHKLock {
      * 模拟线程队列 main线程先进队列,new Thread后进队列,自旋等待
      */
     public static void main(String[] args) throws InterruptedException {
-        final CHKLock lock = new CHKLock();
+        final CLHLock lock = new CLHLock();
 
         lock.lock(); //线程1获取了锁
         System.out.println("线程1获取锁成功");
