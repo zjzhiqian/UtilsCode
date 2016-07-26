d522a238eac42e5cccdb2e858dbaea7ff3d3ea6b
hzq <hzq@hzqdeMacBook-Air.local>
2016-07-26 17:39:19 +0800

    update CLHLock

--- src/test/java/com/hzq/lock/CHKLock.java
+++ src/test/java/com/hzq/lock/CHKLock.java
@@ -1,30 +1,31 @@
 package com.hzq.lock;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
-
-/**
- * Created by hzq on 16/5/14.
- */
-
-
-class QNode {
-    public volatile boolean locked = false;
-}
 
 public class CHKLock {
 
-    private AtomicReference<QNode> atomicRef = new AtomicReference();
-    private ThreadLocal<QNode> mNode;
-    private ThreadLocal<QNode> pNode;
+    /**
+     * Created by hzq on 16/5/14.
+     */
+
+
+    class QNode {
+        public volatile boolean locked = false;
+    }
+
+    private AtomicReference<QNode> tail = new AtomicReference(new QNode());
+    private ThreadLocal<QNode> myNode;
+    private ThreadLocal<QNode> myPre;
 
     public CHKLock() {
-        mNode = new ThreadLocal<QNode>() {
+        myNode = new ThreadLocal<QNode>() {
             @Override
             protected QNode initialValue() {
                 return new QNode();
             }
         };
-        pNode = new ThreadLocal<QNode>() {
+        myPre = new ThreadLocal<QNode>() {
             @Override
             protected QNode initialValue() {
                 return null;
@@ -33,17 +34,39 @@ public class CHKLock {
     }
 
     public void lock() {
-        QNode qNode = mNode.get();
+        QNode qNode = myNode.get();
         qNode.locked = true;
-        QNode refP = atomicRef.getAndSet(qNode);
-        pNode.set(refP);
+        QNode refP = tail.getAndSet(qNode);
+        myPre.set(refP);
         while (refP.locked) {
         }
     }
 
     public void unLock() {
-        QNode qnode = mNode.get();
+        QNode qnode = myNode.get();
         qnode.locked = false;
-        mNode.set(pNode.get());
+        myNode.set(myPre.get());
+    }
+
+
+    /**
+     * 模拟线程队列 main线程先进队列,new Thread后进队列,自旋等待
+     */
+    public static void main(String[] args) throws InterruptedException {
+        final CHKLock lock = new CHKLock();
+
+        lock.lock(); //线程1获取了锁
+        System.out.println("线程1获取锁成功");
+        new Thread(()->{
+            System.out.println("线程2尝试获取锁");
+            lock.lock();  //线程2获取锁会等待
+            System.out.println("线程2获取锁成功");
+        }).start();
+
+        //等待睡眠
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println("线程1释放锁");
+        lock.unLock();
+
     }
 }
